16aa4fd87eb791d8747e4bade0085cdad6ee6983
John Carlos De Armas <dearmas.john1996@gmail.com>
2018-08-25 22:57:47 -0400

    start adding recursive calling

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 
-`#copy .*rc files to the home directory` \
-cp .*rc ~
+`move to local repo directory` \
+cd ~/Documents/git/dotfiles
 
+
+`pull latest commit from github` \
+git pull origin master
+
+
+`#copy .*rc files to the home directory` \
+cp .*rc ~/
 
 `# place vim colors in runtime folder` \
 vimout=$(vim --version)
@@ -12,7 +19,8 @@ touch test
 echo $vimLineOut > test
 sed -i 's/.*".//' test
 sed -i 's/"//' test
-vimXXpath=($cat test)
+vimXXpath=$(cat test)
 rm test
 vimcolorspath="/$vimXXpath/colors"
-
+touch test
+cp test $vimcolorspath
