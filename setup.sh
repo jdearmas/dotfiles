a9a5bf434d48f465cebe6a8c3a74772d7c20fe34
John Carlos De Armas <dearmas.john1996@gmail.com>
2018-08-26 11:33:06 -0400

    comment out git pull

--- setup.sh
+++ setup.sh
@@ -6,21 +6,23 @@ cd ~/Documents/git/dotfiles
 
 
 `pull latest commit from github` \
-git pull origin master
+`git pull origin master`\
 
 
 `#copy .*rc files to the home directory` \
 cp .*rc ~/
 
+
 `# place vim colors in runtime folder` \
 vimout=$(vim --version)
 vimLineOut=$(grep "VIMRUNTIME:" <<< "$vimout")
 touch test
+echo $vimLineOut
 echo $vimLineOut > test
 sed -i 's/.*".//' test
 sed -i 's/"//' test
 vimXXpath=$(cat test)
 rm test
-vimcolorspath="/$vimXXpath/colors"
+vimcolorspath=/$vimXXpath/colors
 touch test
 cp test $vimcolorspath
