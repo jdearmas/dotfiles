3a6ff10bf8345f2fa22008e08026a6527ec68808
John Carlos De Armas <dearmas.john1996@gmail.com>
2018-11-02 14:39:30 -0400

    setup.sh: added option 2 to input argument list. Only updates once per call. It does not set up automatic updating.

--- setup.sh
+++ setup.sh
@@ -1,38 +1,59 @@
 #!/bin/bash
 
+function cd_copy_and_replace(){
+	#move to local repo directory
+	#copy .*rc files to the home directory
+	#pull latest commit from github
+
+	cd ~/Documents/git/dotfiles
+	git pull origin master
+	cp .*rc ~/ ;}
+
+function place_vim_colors(){
+    vimout=$(vim --version)
+    vimLineOut=$(grep "VIMRUNTIME:" <<< "$vimout")
+    touch test
+    echo $vimLineOut > test
+    sed -i 's/.*".//' test
+    sed -i 's/"//' test
+    vimXXpath=$(cat test)
+    rm test
+    vimcolorspath=/$vimXXpath/colors
+    touch test
+    cp vim/space.vim $vimcolorspath ;}
+
 if [ $1 -eq 1 ]
 then
-	#move to local repo directory
-	cd ~/Documents/git/dotfiles
 
-
-	#pull latest commit from github
-	git pull origin master
-
-
-	#copy .*rc files to the home directory
-	cp .*rc ~/
-
+    cd_copy_and_replace
 
 	# place vim colors in runtime folder
-		vimout=$(vim --version)
-		vimLineOut=$(grep "VIMRUNTIME:" <<< "$vimout")
-		touch test
-		echo $vimLineOut > test
-		sed -i 's/.*".//' test
-		sed -i 's/"//' test
-		vimXXpath=$(cat test)
-		rm test
-		vimcolorspath=/$vimXXpath/colors
-		touch test
-		cp vim/space.vim $vimcolorspath
+    place_vim_colors
 
-		# no recursive
-		sed -i '2s/1/0/g' ~/.bashrc
+    # no recursive
+    sed -i '2s/1/0/g' ~/.bashrc
 
-		# source rc's
-		source ~/.*rc
+    # source rc's
+    source ~/.*rc
 
-		# reset
-		sed -i '2s/0/1/g' ~/.bashrc
+    # reset
+    sed -i '2s/0/1/g' ~/.bashrc
+fi
+
+if [ $1 -eq 2 ]
+then
+	#move to local repo directory
+	#pull latest commit from github
+	#copy .*rc files to the home directory
+    cd_copy_and_replace
+
+	# place vim colors in runtime folder
+    place_vim_colors
+
+    # no recursive
+    sed -i '2s/1/0/g' ~/.bashrc
+    sed -i '2s/2/0/g' ~/.bashrc
+
+    # source rc's
+    source ~/.*rc
 fi
