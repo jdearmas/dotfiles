eca710ce7f005d044baab65debd77c6de245da1b
John De Armas <dearmas.john1996@gmail.com>
2019-05-09 17:01:38 +0000

    setup: puts bash, vim, and profile config files in user's home

--- setup.sh
+++ setup.sh
@@ -12,7 +12,12 @@ EDITOR="vim"
     cp .vimrc ~/
 
 # shell
-    if [ ! -d ~/.config/$SHELL ]
+    if [ ! -d ~/.config/$SHELL ]; then
        mkdir ~/.config/$SHELL
-       cp -r shells/$SHELL/.config/ ~/.config/$SHELL/
+       cp -r shells/$SHELL/.config/* ~/.config/$SHELL/
+    fi
+
+# profile
+    cp .profile ~/
+
 
