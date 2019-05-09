29538b9bd57ded218921262e6dce22bb78bc57ca
John De Armas <dearmas.john1996@gmail.com>
2019-05-09 12:01:36 -0400

    created editor/ folder

--- setup.sh
+++ setup.sh
@@ -5,3 +5,14 @@
 #cp $shell configs to $home
 
 
+SHELL="bash"
+EDITOR="vim"
+
+# editor
+    cp .vimrc ~/
+
+# shell
+    if [ ! -d ~/.config/$SHELL ]
+       mkdir ~/.config/$SHELL
+       cp -r shells/$SHELL/.config/ ~/.config/$SHELL/
+
